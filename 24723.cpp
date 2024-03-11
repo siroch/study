c360e032b9af779b69078a7849688f14e9f11d42
siroch <habizero@gmail.com>
2024-03-11 20:12:10 +0900

    240311 단계별풀이 19

---
+++ 24723.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+  ios_base :: sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+  int N; cin>>N;
+  cout<<pow(2,N);
+  return 0;
+}
