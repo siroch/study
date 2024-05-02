1efeb725a162859d5da44a87b62502348ac7090a
siroch <habizero@gmail.com>
2024-05-02 17:52:03 +0900

    240502 dp

---
+++ 9251.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  ios_base :: sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+  string s1,s2; cin>>s1>>s2;
+
+  int len1 = s1.length();
+  int len2 = s2.length();
+
+  vector<vector<int>> dp(len1+1, vector<int>(len2+1, 0));
+
+  for(int i=1; i<=len1; i++){
+    for(int j=1; j<=len2; j++){
+      if(s1[i-1] == s2[j-1]) dp[i][j] = dp[i-1][j-1] + 1; 
+      else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+    }
+  }
+  cout<<dp[len1][len2]<<"\n";
+  return 0;
+}
